071d0c9296c06cdd11db10534abeddd4e842a3ab
benz <ref4gha@gmail.com>
2025-03-14 20:29:18 +0700

    [REFACTOR]: optimized method to find highest gpa student to just fetch one student instead of all

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
@@ -1,12 +1,32 @@
 package com.advpro.profiling.tutorial.repository;
 
+
+
 import com.advpro.profiling.tutorial.model.Student;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.stereotype.Repository;
 
+
+
 /**
+
  * @author muhammad.khadafi
+
  */
+
 @Repository
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+
+    @Query("SELECT s FROM Student s ORDER BY s.gpa DESC LIMIT 1")
+
+
+    Student findTopByOrderByGpaDesc();
+
 }
